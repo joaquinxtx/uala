5328521526dd0ef0089fa122487512d79d68cdd0
jtoledo <jtoledo@ntbnet.com.ar>
2025-05-27 15:03:50 -0300

    chore(hilt): agrega configuración base de Hilt con clase UalaApp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.kapt")
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -56,4 +59,32 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //ROOM
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.ksp)
+    testImplementation(libs.room.testing)
+    // Dagger core
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+    // Hilt DI
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    // Hilt para Jetpack Compose
+    implementation(libs.hilt.navigation.compose)
+    //Retrofit , moshi y OkHttp
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.moshi)
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.kotlin.codegen)
+    implementation(libs.okhttp)
+    // Unit testing
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    // Mocking
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.truth)
 }
--- app/src/main/java/com/joaquin/uala/presentation/MainActivity.kt
+++ app/src/main/java/com/joaquin/uala/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.joaquin.uala
+package com.joaquin.uala.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,13 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.joaquin.uala.ui.theme.UalaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UalaTheme {
+            UalaTheme  {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
